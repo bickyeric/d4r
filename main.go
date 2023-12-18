d517a87fab415a0afd6bea4db3d272564eca1779
Bicky Eric Kantona <bickyeric@proton.me>
2023-12-18 23:55:43 +0700

    init cobra project

--- main.go
+++ main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
-)
+import "gitlab.com/bickyeric/d4r/cmd"
 
 func main() {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		panic(err)
-	}
-
-	arr, err := cli.ImageList(context.Background(), types.ImageListOptions{
-		All: true,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, i := range arr {
-		fmt.Printf("%s %v\n", i.ID, i)
-	}
+	cmd.Execute()
 }
