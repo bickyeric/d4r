3ec0cc409c17f859f65920168fa27fcfc174abb1
Bicky Eric Kantona <bickyeric@proton.me>
2024-02-25 21:55:41 +0700

    kill and format port

--- view/container.go
+++ view/container.go
@@ -3,6 +3,8 @@ package view
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types"
@@ -26,7 +28,7 @@ func NewContainer(app *App) *Container {
 func (c *Container) init() {
 	c.reloadData()
 	c.SetSelectable(true, false)
-	c.SetBorder(true).SetTitle(fmt.Sprintf(" %s [%d] ", "Container", c.GetRowCount()))
+	c.SetBorder(true)
 
 	c.bindKeys()
 }
@@ -43,18 +45,35 @@ func (c *Container) reloadData() {
 
 	c.content = containers
 
-	for i, name := range []string{"CONTAINER ID", "NAMES", "IMAGE", "COMMAND", "CREATED", "PORTS"} {
+	for i, name := range []string{"CONTAINER ID", "NAMES", "IMAGE", "COMMAND", "CREATED", "STATUS", "PORTS"} {
 		c.SetCell(0, i, tview.NewTableCell(name).SetSelectable(false))
 	}
 
 	for index, container := range c.content {
 		c.SetCell(index+1, 0, tview.NewTableCell(container.ID[7:19]))
-		c.SetCell(index+1, 1, tview.NewTableCell(fmt.Sprint(container.Names[0][1:])))
+		c.SetCell(index+1, 1, tview.NewTableCell(container.Names[0][1:]))
 		c.SetCell(index+1, 2, tview.NewTableCell(container.Image))
-		c.SetCell(index+1, 3, tview.NewTableCell(container.Command))
-		c.SetCell(index+1, 4, tview.NewTableCell(fmt.Sprint(time.Unix(container.Created, 0))))
-		c.SetCell(index+1, 5, tview.NewTableCell(fmt.Sprint(container.Ports)))
+
+		if len(container.Command) > 19 {
+			c.SetCell(index+1, 3, tview.NewTableCell(strconv.Quote(fmt.Sprintf("%s…", container.Command[:19]))))
+		} else {
+			c.SetCell(index+1, 3, tview.NewTableCell(strconv.Quote(container.Command)))
+		}
+
+		createdAt := time.Unix(container.Created, 0)
+		createdDuration := time.Since(createdAt).Round(time.Second)
+		c.SetCell(index+1, 4, tview.NewTableCell(createdDuration.String()+" ago"))
+
+		c.SetCell(index+1, 5, tview.NewTableCell(container.Status))
+
+		var ports []string
+		for _, port := range container.Ports {
+			ports = append(ports, c.formatPort(port))
+		}
+		c.SetCell(index+1, 6, tview.NewTableCell(strings.Join(ports, ", ")))
 	}
+
+	c.SetTitle(fmt.Sprintf(" %s [%d] ", "Container", c.GetRowCount()))
 }
 
 func (c *Container) bindKeys() {
@@ -62,6 +81,7 @@ func (c *Container) bindKeys() {
 
 	c.app.keybordHandlers["a"] = NewAction("View All", c.toggleViewAll)
 	c.app.keybordHandlers["d"] = NewAction("Delete Container", c.delete)
+	c.app.keybordHandlers["k"] = NewAction("Kill Container", c.kill)
 }
 
 func (c *Container) toggleViewAll() error {
@@ -79,4 +99,32 @@ func (c *Container) delete() error {
 	}
 	c.reloadData()
 	return nil
+}
+
+func (c *Container) kill() error {
+	selectedRow, _ := c.Table.GetSelection()
+	selectedContainer := c.content[selectedRow-1]
+
+	go func(container types.Container) {
+		err := c.app.docker.ContainerKill(context.Background(), container.ID, "")
+		if err != nil {
+			c.app.logger.Println(err)
+		}
+	}(selectedContainer)
+
+	return nil
+}
+
+func (c *Container) formatPort(p types.Port) string {
+	var s string
+	if p.IP != "" {
+		s = s + p.IP + ":"
+	}
+
+	if p.PublicPort > 0 {
+		s = s + fmt.Sprint(p.PrivatePort, "->", p.PublicPort)
+	} else {
+		s = s + fmt.Sprint(p.PrivatePort)
+	}
+	return s + "/" + p.Type
 }
